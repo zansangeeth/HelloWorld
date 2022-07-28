482c8f48485a9f2f2861b568d024d18cffeda0d4
zansangeeth <zansangeeth17@gmail.com>
2022-07-28 14:50:41 +0530

    added button onclicked listener

--- app/src/main/java/com/zasa/helloworld/MainActivity.kt
+++ app/src/main/java/com/zasa/helloworld/MainActivity.kt
@@ -2,10 +2,21 @@ package com.zasa.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Log.i(TAG, "Button Clicked!")
+            Toast.makeText(this, "Hello to you to too!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
